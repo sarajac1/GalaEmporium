33520b4fab0ac6c4d55c47716aa119a909765d9e
nighthail <natthagelmail@gmail.com>
2023-12-06 10:39:58 +0100

    Started reorganizing our site to reflect page turner navigation

--- client/router.js
+++ client/router.js
@@ -2,6 +2,34 @@
 import login from "./components/login.js"
 $('#login').html(login())
 
+//main page
+import home from "./pages/home.js"
+
+//vampire club
+import vampireclub from "./pages/vampireclub.js"
+
+// routed pages
+async function router() {
+  switch (location.hash) {
+
+    case "":
+      $('main').html(home())
+      break
+
+    case "#vampireclub":
+      $('main').html(vampireclub())
+      break
+
+    default:
+      $('main').html(`<h2><strong>404</strong> Good job! You've broken the internet.</h2>`)
+
+  }
+}
+
+// event handlers, calls the router function on each event (change, load)
+window.onhashchange = router
+window.onload = router
+
 
 // event handlers, calls the router function on each event (change, load)
 // window.onhashchange = router
