2b24bfcce3501eaf5223e1fa5f29d2b08e5a310d
Rajesh <149164814+RJKN13@users.noreply.github.com>
2023-12-12 09:12:20 +0100

    Created api to post new event in a club.

--- server/apis/events.js
+++ server/apis/events.js
@@ -11,4 +11,26 @@ export default function (server, db) {
     }    
       res.json(events)    
     })
+
+
+  server.post('/api/events', async (req, res) => {
+    const title = req.body.title.trim()
+    const description = req.body.description
+    const starts_at = req.body.starts_at
+    const ends_at = req.body.ends_at
+    const created_by = req.body.created_by
+    const club_name = req.body.club_name
+
+    if (req.body.title.trim().length > 0) {
+      const result = await db.query("INSERT INTO events (title, description, starts_at, ends_at," + 
+        "created_by, club_name) VALUES (?, ?, ?, ?, ?, ?)",
+        [title, description, starts_at, ends_at, created_by, club_name])
+      result.eventAdded = true
+      res.json(result)
+      console.log("Result - ", result);
+    } else {
+      res.status(401)
+      res.json({ eventAdded: false })
+    }
+  })
 }
