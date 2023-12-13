eb0381cb44ac21d3f5273fa0bcfb36eef619d379
sarajac1 <sarajac@hotmail.com>
2023-12-13 11:37:43 +0100

    Made changes to homepage, added descriptions for all clubs

--- client/pages/home.js
+++ client/pages/home.js
@@ -16,19 +16,19 @@ export default async function home() {
           <div class="row">
           <div class="borderimg">
             <a class="magic-show" href="#magicshowclub">
-              <img src="img/magic-show.jpg" alt="">Do you love magic? Of course you do! Then The Great Magic Show is for you!
+              <img src="img/magic-show.jpg" alt="">Magic show club <br> Do you love magic? Of course you do! Then The Great Magic Show is for you!
             </a>
           </div>
 
           <div class="borderimg">
             <a class="dancestage" href="#danceclub">
-              <img src="img/dance_stage.jpg" alt="">Dance club
+              <img src="img/dance_stage.jpg" alt="">Dance Club <br>Where rhythm meets expression in a vibrant dance sanctuary
             </a>
           </div>
 
           <div class="borderimg">
             <a class="vampireclub" href="#vampireclub">
-              <img src="img/eclipsed_embrace.jpg" alt="">Vampire club
+              <img src="img/eclipsed_embrace.jpg" alt="">Vampire club <br> A sanctuary blurring mortal and immortal boundaries, inviting you into the shadows
             </a>
           </div>
         </div>
@@ -36,14 +36,14 @@ export default async function home() {
         <div class="row">
           <div class="borderimg">
             <a class="fancy-club" href="#fancyclub">
-              <img src="https://cdn.vox-cdn.com/thumbor/V0iHI3a8eg77wf87m7HYKl1zQCU=/0x0:2400x1602/920x0/filters:focal(0x0:2400x1602):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/24575363/PearlClubInteriors_digital_6817.jpg" alt="">Our web platform serves as a portal to the dynamic
+              <img src="https://cdn.vox-cdn.com/thumbor/V0iHI3a8eg77wf87m7HYKl1zQCU=/0x0:2400x1602/920x0/filters:focal(0x0:2400x1602):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/24575363/PearlClubInteriors_digital_6817.jpg" alt="">Fancy Club <br> Our web platform serves as a portal to the dynamic
               world of live entertainment.
             </a>
           </div>
 
           <div class="borderimg">
             <a class="cowboy-club" href="#cowboyclub">
-              <img src="img/cowboylogo.png" alt="cowboylogo">Cowboy Club
+              <img src="img/cowboylogo.png" alt="cowboylogo">Cowboy Club <br> Join us for frontier tales and camaraderie. Saddle up for adventure!
             </a>
           </div>
 
